d19fe228dd9baad6273b983501e8d54316d77c52
Kyle Robinson <robin489@purdue.edu>
2024-01-17 17:36:39 -0500

    Added a new line to covReport

--- summarizeCoverage.py
+++ summarizeCoverage.py
@@ -28,5 +28,5 @@ if __name__ == "__main__":
     jacoco_xml_path = sys.argv[1] + "target/site/jacoco/jacoco.xml"# Replace with the actual path to your jacoco.xml
     line_coverage_percentage = get_line_coverage_percentage(jacoco_xml_path)
     with open("CovReport.txt", 'a') as file:
-        file.write(f"Line Coverage Percentage: {line_coverage_percentage:.2f}%")
+        file.write(f"Line Coverage Percentage: {line_coverage_percentage:.2f}%\n")
     print(f"Line Coverage Percentage: {line_coverage_percentage:.2f}%")
