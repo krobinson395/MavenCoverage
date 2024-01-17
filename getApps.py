9825e19bf763d422d984bf3d52dee44dc124c595
Kyle Robinson <robin489@purdue.edu>
2024-01-17 16:32:54 -0500

    Added script to get top apps from DB

---
+++ getApps.py
@@ -0,0 +1,43 @@
+import psycopg2
+
+
+def getTopURLS():
+    try:
+        connection = psycopg2.connect(
+            user="postgres",
+            password="postgres",
+            host="localhost",
+            port="5432",
+            database="packages_production"
+        )
+    
+        cursor = connection.cursor()
+        
+        # Execute the query and fetch the first 1000 results
+        #The 2482 number is due to duplicates in the db the resulting set is 1000 results
+        query = """SELECT repository_url
+    FROM (
+        SELECT DISTINCT repository_url, dependent_packages_count
+        FROM packages
+        WHERE ecosystem LIKE 'maven' AND repository_url LIKE '%github%'
+    ) AS subquery
+    ORDER BY dependent_packages_count DESC
+    LIMIT 200;"""
+    
+        cursor.execute(query)
+    
+        # Fetch all the results
+        rows = cursor.fetchall()
+        github_urls = list(set(rows))
+        
+        print("Finished db query")
+        return(github_urls)
+    except psycopg2.Error as e:
+        print(f"Error with db service: {e}")
+        return(None)
+    
+if __name__ == "__main__":
+    github_urls = getTopURLS()
+    for url in github_urls:
+        print(f"Current URL: {url}")
+    print(f"Total URLS {len(github_urls}")
